3615a38b0c0053a9d452047a70774203c359227d
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2023-02-04 16:19:12 +0300

    tic-tac-toe-two-assholes-in-the-house

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as Button).Name == "b10")
+            {
+                ResetGame();
+            }
+            else
+            {
+                (sender as Button).Content = "X";
+                (sender as Button).IsEnabled = false;
+                if (CheckForWinner())
+                {
+                    return;
+                }
+                Robot();
+            }
+
+        }
+
+        private void Robot()
+        {
+            Button[] buttons = new Button[] { b1, b2, b3, b4, b5, b6, b7, b8, b9 };
+            Random r = new Random();
+            int jopa;
+            do
+            {
+                jopa = r.Next(0, 9);
+            } while (!(buttons[jopa].Content == "" || buttons[jopa].Content == null));
+            buttons[jopa].Content = "O";
+            buttons[jopa].IsEnabled = false;
+            if (CheckForWinner())
+            {
+                return;
+            }
+        }
+
+        private bool CheckForWinner()
+        {
+            Button[] buttons = new Button[] { b1, b2, b3, b4, b5, b6, b7, b8, b9 };
+            string[] lines = new string[]
+            {
+                b1.Content.ToString() + b2.Content.ToString() + b3.Content.ToString(),
+                b4.Content.ToString() + b5.Content.ToString() + b6.Content.ToString(),
+                b7.Content.ToString() + b8.Content.ToString() + b9.Content.ToString(),
+                b1.Content.ToString() + b4.Content.ToString() + b7.Content.ToString(),
+                b2.Content.ToString() + b5.Content.ToString() + b8.Content.ToString(),
+                b3.Content.ToString() + b6.Content.ToString() + b9.Content.ToString(),
+                b1.Content.ToString() + b5.Content.ToString() + b9.Content.ToString(),
+                b3.Content.ToString() + b5.Content.ToString() + b7.Content.ToString()
+            };
+
+            foreach (var line in lines)
+            {
+                if (line == "XXX")
+                {
+                    MessageBox.Show("ХРОМОСОМА ВЫЙГРАЛА");
+                    ResetGame();
+                    return true;
+                }
+                else if (line == "OOO")
+                {
+                    MessageBox.Show("НОЛИК ВЫЙГРАЛ");
+                    ResetGame();
+                    return true;
+                }
+            }
+            if (!buttons.Any(b => b.Content == "" || b.Content == null))
+            {
+                MessageBox.Show("ЗАЧЕМ ИГРАТЬ В ТАКИЕ ИГРЫ");
+                ResetGame();
+                return true;
+            }
+            return false;
+        }
+
+        private void ResetGame()
+        {
+            b1.Content = "";
+            b1.IsEnabled = true;
+            b2.Content = "";
+            b2.IsEnabled = true;
+            b3.Content = "";
+            b3.IsEnabled = true;
+            b4.Content = "";
+            b4.IsEnabled = true;
+            b5.Content = "";
+            b5.IsEnabled = true;
+            b6.Content = "";
+            b6.IsEnabled = true;
+            b7.Content = "";
+            b7.IsEnabled = true;
+            b8.Content = "";
+            b8.IsEnabled = true;
+            b9.Content = "";
+            b9.IsEnabled = true;
+
+
+        }
+    }
+}
+/*
+Крестики нолики, две БЛЯДИНЫ в домике
+Укатились даже шарики за ролики
+ЖОПА-ЖОПА ПЕНИС ЧЛЕН, разгоняя ШЛЮШКИ
+Люди обзываются, что мы две РУКОБЛУДКИ
+Отпути, не путю, но кому я ХУЙ ДРОЧУ
+Не путю ведь я люблю свою лучшую ЗАЛУПУ
+РКОБЛЮД ПЕНИС ХЕР, ЖОПА ШЛЮХА МАЛОФЬЯ, но кому я говорю
+Не путю, ведь я люблю, навсегда мы друг для друга
+ГОВНО, ГОВНО, ГОВНО, ГОВНО, ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ ХУЙ 
+
+Эники-беники, в ЖОПЕ и на телеке
+Мы ГОВНО доводим топы до истерики
+РУКАБЛУД ВЛАГАЛИЩЕ,РУКАБЛУД МАШОНКА
+Люди обзываются, ХУЙ ГАЛОВКА
+Отпути, не путю, но кому я ХУЙ ДРОЧУ
+Не путю ведь я люблю свою лучшую ЗАЛУПУ
+РКОБЛЮД ПЕНИС ХЕР, ЖОПА ШЛЮХА МАЛОФЬЯ, но кому я говорю 
+Не путю, ведь я люблю, навсегда мы друг для друга
+ЯЛДА, ЯЛДА,ЯЛДА,ЯЛДА,ЯЛДА,ЯЛДААААААААААААА, ЧЛЕН, ЧЛЕН,ЧЛЕН,ЧЛЕН,ЧЛЕН
+У нас с тобой свой ПЕРДУН
+Нам не надо ПЕНИС ХЕР
+Повсюду за мной везде 
+Моя лучшая подруга
+МАНДА МАНДА МАНДА МАНДААААА
+РАУНД*/
